06641fd91de60cb2884273a08ff3d405201b22c9
Jaqueline Rossetto <44752240+jaquerossetto@users.noreply.github.com>
2022-06-09 17:10:01 -0300

    arquivo cs do desafio

---
+++ Program2.cs
@@ -0,0 +1,32 @@
+﻿
+string retornarDiagnosticoDoUsuario(double alturaDoUsuario, double pesoDoUsuario)
+{
+    double IMC = pesoDoUsuario / (alturaDoUsuario * alturaDoUsuario);
+    string diagnostico = "";
+   
+
+    if(IMC < 18.5)
+    {
+        diagnostico = "Anemico";
+    }else if(IMC >= 18.5 && IMC <= 24.9)
+    {
+        diagnostico = "Normal";
+    }else if (IMC >= 25 && IMC <= 29.9)
+    {
+        diagnostico = "Sobrepeso";
+    }else if (IMC >= 30 && IMC <= 40)
+    {
+        diagnostico = "Obeso";
+    }
+    else
+    {
+        diagnostico = "Obesidade grave";
+    } 
+    return diagnostico;
+} 
+
+    Console.WriteLine("Digite a altura do usuário:");
+    double.TryParse(Console.ReadLine(), out double alturaDoUsuario);
+    Console.WriteLine("Digite o peso do usuário:");
+    double.TryParse(Console.ReadLine(), out double pesoDoUsuario);
+    Console.WriteLine($"Seu diagnóstico é {retornarDiagnosticoDoUsuario(alturaDoUsuario,pesoDoUsuario)}");
